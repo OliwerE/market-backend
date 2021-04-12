630e62a2fe12fd16f93c29b64ef158bcb5c44ea8
Oliwer <oe222ez@student.lnu.se>
2021-04-12 13:48:54 +0200

    response tests

--- src/server.js
+++ src/server.js
@@ -14,6 +14,11 @@ const server = async () => {
   app.use(logger('dev'))
   app.use(express.json())
 
+  app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+next();
+});
+
   app.use('/', router)
 
   app.use((err, req, res, next) => {
