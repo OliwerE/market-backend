20fb360b426f224d5e0c04eccead6e77b4204806
Oliwer <oe222ez@student.lnu.se>
2021-06-05 20:13:45 +0200

    changed csrf cookie config

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -41,7 +41,7 @@ export class AuthController {
    */
   getCsrfToken (req, res, next) {
     // res.json({ csrfToken: req.csrfToken() })
-    res.cookie('XSRF-TOKEN', req.csrfToken(), { secure: true })
+    res.cookie('XSRF-TOKEN', req.csrfToken(), { sameSite: 'none' })
     res.json({})
   }
 
